48aea78d17d537b719fe789b19dbcdb45230e6f1
Marinus Swanepoel <marinus@noweverywhere.ca>
2022-07-07 22:14:49 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Cell from './components/Cell';
 import './App.css'
 
 function App() {
+  const [winner, setWinner] = useState(null);
 
   const [activePlayer, setActivePlayer] = useState('x');
 
@@ -14,6 +15,25 @@ function App() {
 
   const [oCells, setOCells] = useState([]);
 
+  useEffect(() => {
+    
+  });
+
+  function analyzeForAWin(playerArray) {
+    const rows = [[1,2,3], [4,5,6], [7,8,9]];
+    const columns = [[1,4,7], [2,5,8], [3,6,9]];
+    const diagonals = [[1,5,9], [3,5,6]];
+    if (
+      rows.some(row => row.every(x => playerArray.includes(x))) ||
+      columns.some(column => column.every(x => playerArray.includes(x))) ||
+      diagonals.some(diag => diag.every(x => playerArray.includes(x)))
+    ) {
+      debugger
+      setWinner(activePlayer)
+    } else {
+      console.log(playerArray)
+    }
+  }
 
   const cells = [1,2,3,4,5,6,7,8,9];
 
@@ -26,11 +46,13 @@ function App() {
   const handleClick = (cell) => {
     if(!takenCells.includes(cell)) {
       setTakenCells(prevCell => [...prevCell, cell]);
-      if(activePlayer === 'x') {
+      if (activePlayer === 'x') {
         setXCells(prevCell => [...prevCell, cell]);
+        analyzeForAWin(xCells);
         setActivePlayer('o');
       } else if (activePlayer === 'o') {
         setOCells(prevCell => [...prevCell, cell]);
+        analyzeForAWin(oCells);
         setActivePlayer('x');
       }
     }
@@ -38,6 +60,7 @@ function App() {
 
   return (
     <div className="App">
+      {winner && `${winner} won`}
       <div className="grid">
         {cells.map(cell => {
           return (
